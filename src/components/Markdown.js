d29037cda7f30e6b9504527bf52a6997f1a110ff
Matthew McIntyre <mattymac2@gmail.com>
2019-03-12 08:34:28 +0000

    styling updates and responsiveness

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const Remarkable = require("remarkable");
 
 const defaultValue =
-  "# Main Header \n## sub header \n[link to my portfolio](https://matthewmcintyre.me)\n\nInline `code`\n```\nblock of code\n```\n list\n1. first\n2. second\n3. third\n\nblockquotes\n> can\n>> be\n>>> nested\n\n![react image](https://octodex.github.com/images/daftpunktocat-guy.gif)\n\n**bolded text**";
+  "# Main Header \n\n## sub header \n\n[link to my portfolio](https://matthewmcintyre.me)\n\nInline `code`\n\n```\nblock of code\n```\n list\n1. first\n2. second\n3. third\n\nblockquotes\n> can\n>> be\n>>> nested\n\n![react image](https://octodex.github.com/images/daftpunktocat-guy.gif)\n\n**bolded text**";
 
 class Markdown extends Component {
   constructor(props) {
